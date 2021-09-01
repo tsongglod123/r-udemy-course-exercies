f8671b01c1f3e86523757e3ca7726575909898ab
tan <t.songglod@gmail.com>
2021-09-02 04:14:08 +0700

    [-] remove unnecessary lines

--- Exercises/R Basics Exercise.R
+++ Exercises/R Basics Exercise.R
@@ -1,26 +1,20 @@
 #ex 1
 print(2^5)
-print("=============================")
 
 #ex 2
 stock_prices <- c(23, 27, 23, 21, 34)
 print(stock_prices)
-print("=============================")
 
 #ex 3
 names(stock_prices) <- c("Mon", "Tue", "Wed", "Thu", "Fri")
 print(stock_prices)
-print("=============================")
 
 #ex 4
 print(mean(stock_prices))
-print("=============================")
 
 #ex 5
 over_23 <- stock_prices[stock_prices > 23]
 print(over_23)
-print("=============================")
 
 #ex 6
 print(max(stock_prices))
-print("=============================")
--- Exercises/R Matrix Exercises.R
+++ Exercises/R Matrix Exercises.R
@@ -3,34 +3,26 @@ va <- 1:3
 vb <- 4:6
 vect_matrix <- rbind(va, vb)
 print(vect_matrix)
-print("=============================")
 
 #ex 2
 mat <- matrix(1:9, nrow = 3, ncol = 3, byrow = TRUE)
 print(mat)
-print("=============================")
 
 #ex 3
 print(is.matrix(mat))
-print("=============================")
 
 #ex 4
 mat2 <- matrix(1:25, nrow = 5, ncol = 5, byrow = TRUE)
 print(mat2)
-print("=============================")
 
 #ex 5
 print(mat2[2:3, 2:3])
-print("=============================")
 
 #ex 6
 print(mat2[4:5, 4:5])
-print("=============================")
 
 #ex 7
 print(sum(mat2))
-print("=============================")
 
 #ex 8
-print(matrix(runif(20, min = 0, max = 100), nrow = 4, ncol = 5))
-print("=============================")
+print(matrix(round(runif(20, min = 0, max = 100), 2), nrow = 4, ncol = 5))
